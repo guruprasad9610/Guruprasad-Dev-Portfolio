45004b643d041b0a60c3791f1865c2deb14f823c
GURU PRASAD SAHU <guruprasad9610@gmail.com>
2023-11-01 17:13:03 +0530

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://linktr.ee/guruprasad9610",
   resume:
-    "https://drive.google.com/file/d/1kDELSwlfNBFKx_CXyR0ypfFxtSq5sp5q/view?usp=sharing",
+    "https://drive.google.com/file/d/1rO3ZEsOjNEzq0s03GYY_ZjFrcrvxJQW0/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/guruprasad9610",
   twitter: "https://twitter.com/GURUPRASAD9610",
   insta: "https://instagram.com/guruprasadsahu9610",
